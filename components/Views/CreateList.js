7e970801e7a0ed8a7632b8b6381efddc5dbb59c5
Brady Snuggs <dronebug360@gmail.com>
2020-03-10 23:37:24 -0600

    finish using asyncStorage and implement animation.

--- components/Views/CreateList.js
+++ components/Views/CreateList.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {View, Text} from 'react-native'
 
 
 const CreateOrEditList = () => {
     return(
-
+        <View>
+            <Text>
+            Create or edit page.
+            </Text>
+        </View>
     )
 }
 
